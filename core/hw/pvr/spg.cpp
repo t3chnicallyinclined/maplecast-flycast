sashkinbro/EmuCoreH

commit 244b6ea1316d6cc70a8b1ce764ddb9c6bd89863e

pvr: adaptive end-of-render interrupt delay
EoR interrupt delay now depends on the TA context size.
Issue #634
Fixes issues with FMV in some WinCE games (Resident Evil 2, Next Tetris,
Nightmare Creatures 2)

core/hw/pvr/spg.cpp
@@ -270,7 +270,20 @@ void spg_Reset(bool hard)
 void scheduleRenderDone(TA_context *cntx)
 {
 	if (cntx)
-		sh4_sched_request(render_end_schid, 500000 * 3);
+	{
+		int cycles;
+		if (settings.platform.isNaomi2()) {
+			cycles = 1500000;
+		}
+		else
+		{
+			int size = 0;
+			for (TA_context *c = cntx; c != nullptr; c = c->nextContext)
+				size += c->tad.thd_data - c->tad.thd_root;
+			cycles = std::min(100000 + size * 2, 1500000);
+		}
+		sh4_sched_request(render_end_schid, cycles);
+	}
 	else
 		sh4_sched_request(render_end_schid, 4096);
 }

t3chnicallyinclined/maplecast-flycast

commit c0b23531c2d8aed2d6bf817a17453306d942eae6

pvr: naomi mvsc2 needs more rendering cycles
Regression introduced by 244b6ea1316d6cc70a8b1ce764ddb9c6bd89863e

core/hw/pvr/spg.cpp
@@ -283,7 +283,7 @@ void scheduleRenderDone(TA_context *cntx)
 			int size = 0;
 			for (TA_context *c = cntx; c != nullptr; c = c->nextContext)
 				size += c->tad.thd_data - c->tad.thd_root;
-			cycles = std::min(100000 + size * 2, 1500000);
+			cycles = std::min(200000 + size * 3, 1500000);
 		}
 		sh4_sched_request(render_end_schid, cycles);
 	}