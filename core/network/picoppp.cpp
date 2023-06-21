sashkinbro/EmuCoreH

commit b54f7d659017c56c21cb300299834eb769a8dff9

picotcp: bind native udp socket to the same local port as the pico one
The Toy Racer server ignores the source udp port so it must match the
dreamcast one. Will not work if the port is in use or well-known, or if
NAT gets in the way.
Issue #1112

core/network/picoppp.cpp
@@ -25,6 +25,7 @@
 
 #include "stdclass.h"
 
+//#define BBA_PCAPNG_DUMP
 #ifdef BBA_PCAPNG_DUMP
 #include "oslib/oslib.h"
 #endif
@@ -492,6 +493,16 @@ static sock_t find_udp_socket(uint16_t src_port)
 	int broadcastEnable = 1;
 	setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, (const char *)&broadcastEnable, sizeof(broadcastEnable));
 
+	// bind to same port if possible (Toy Racer)
+	sockaddr_in saddr;
+	socklen_t saddr_len = sizeof(saddr);
+	memset(&saddr, 0, sizeof(saddr));
+	saddr.sin_family = AF_INET;
+	saddr.sin_addr.s_addr = INADDR_ANY;
+	saddr.sin_port = src_port;
+	if (::bind(sockfd, (sockaddr *)&saddr, saddr_len) < 0)
+		perror("bind");
+
 	// FIXME Need to clean up at some point?
 	udp_sockets[src_port] = sockfd;
 
@@ -819,7 +830,6 @@ static pico_device *pico_eth_create()
     return eth;
 }
 
-//#define BBA_PCAPNG_DUMP
 static FILE *pcapngDump;
 
 static void dumpFrame(const u8 *frame, u32 size)

t3chnicallyinclined/maplecast-flycast

commit 283e81f6f652fca766c6a02430c0ba7b3f395a85

network: fix known UDP ports bind regression
introduced by b54f7d659017c56c21cb300299834eb769a8dff9
Add driving strikers netdemo known port
Issue #1112

core/network/picoppp.cpp
@@ -232,6 +232,10 @@ static GamePortList GamesPorts[] = {
 		{ },
 		{ 17219 },
 	},
+	{ // Driving Strikers online demo
+		{ "IND-161053" },
+		{ 30099 },
+	},
 
 	{ // Atomiswave
 		{ "FASTER THAN SPEED" },
@@ -1078,18 +1082,8 @@ static void *pico_thread_func(void *)
 		for (u32 i = 0; i < std::size(ports->udpPorts) && ports->udpPorts[i] != 0; i++)
 		{
 			uint16_t port = short_be(ports->udpPorts[i]);
-			sock_t sockfd = find_udp_socket(port);
-			saddr.sin_port = port;
-
-			if (::bind(sockfd, (sockaddr *)&saddr, saddr_len) < 0)
-			{
-				perror("bind");
-				closesocket(sockfd);
-				auto it = udp_sockets.find(port);
-				if (it != udp_sockets.end())
-					it->second = INVALID_SOCKET;
-				continue;
-			}
+			find_udp_socket(port);
+			// bind is done in find_udp_socket
 		}
 
 		for (u32 i = 0; i < std::size(ports->tcpPorts) && ports->tcpPorts[i] != 0; i++)